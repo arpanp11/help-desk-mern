9aef28a6da6dbb0124356b39a941577ef88871cd
arpanp11 <arpanpatel2810@gmail.com>
2022-01-31 14:04:40 -0500

    setup express framework

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Help Desk.' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
