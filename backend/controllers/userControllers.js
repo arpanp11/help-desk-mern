e88f944abb4cc7b0cd1b418bf3028ac94b7c8ea9
arpanp11 <arpanpatel2810@gmail.com>
2022-01-31 14:53:10 -0500

    added register user feature

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/userModel');
 
 // register a new user
 // route --> /api/users
@@ -10,7 +13,36 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('Please include all fields.');
   }
-  res.send('Register router...');
+
+  // find , if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User alredy exists.');
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // creat a user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data.');
+  }
 });
 
 // login a new user
