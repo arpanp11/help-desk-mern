e0f12db7161c7c874edd6553ea3fbee3f542f7c7
arpanp11 <arpanpatel2810@gmail.com>
2022-01-31 23:37:09 -0500

    connect form with redux

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 
 import { FaUser } from 'react-icons/fa';
 import { toast } from 'react-toastify';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { register } from '../features/auth/authSlice';
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -12,6 +15,12 @@ function Register() {
   });
 
   const { name, email, password, password2 } = formData;
+
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
 
   const changeHandler = (e) => {
     setFormData((prevState) => ({
@@ -25,6 +34,14 @@ function Register() {
 
     if (password !== password2) {
       toast.error('Passwords do not match');
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+
+      dispatch(register(userData));
     }
   };
   return (
