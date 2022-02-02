58403e23f846b5c1cd74695288c063bbdb796d95
arpanp11 <arpanpatel2810@gmail.com>
2022-02-01 22:26:52 -0500

    added delete and update ticket function

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -4,9 +4,18 @@ const router = express.Router();
 const {
   getTickets,
   createTickets,
+  getTicket,
+  deleteTicket,
+  updateTicket,
 } = require('../controllers/ticketControllers');
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/').get(protect, getTickets).post(protect, createTickets);
+
+router
+  .route('/:id')
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
