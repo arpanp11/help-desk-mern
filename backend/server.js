ed6a6798e2a5883bd1e65b06475013dccd966a45
arpanp11 <arpanpatel2810@gmail.com>
2022-02-02 11:45:07 -0500

    setup for prod deployment

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 
-const errorHandler = require('./middleware/errorMiddleware');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
@@ -23,6 +24,21 @@ app.get('/', (req, res) => {
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
 
+// serve frontend
+if (process.env.NODE_ENV === 'production') {
+  // set build folder as static (frontend folder)
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Help Desk API' });
+  });
+}
+
+// route error handler, if there is any
 app.use(errorHandler);
 
 app.listen(PORT, () => {
